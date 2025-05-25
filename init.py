966ac92250095a26cec82549e5af66103895a7ed
Hongtian Wang <wanghongtiancodingoier@gmail.com>
2025-05-25 13:25:38 +0000

    fix: 不需要锁

--- init.py
+++ init.py
@@ -1,14 +1,10 @@
 import os
 import shutil
-import secrets
 
 import chess
 
 
 os.remove('./chess-games/chess.txt')
 shutil.copy('./chess-games/init.txt', './chess-games/chess.txt')
-
-with open('./chess-games/lock.txt', 'w', encoding='utf-8') as f:
-    f.write(secrets.token_hex(4))
 
 chess.updateReadme()
